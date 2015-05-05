c493cee2a2a574a8afb1fe44e351ef55968f401f
Valentin PAUL <valentinpaul04@gmail.com>
2015-05-05 19:55:15 +0200

    truc de merde again

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     //histoire de rajouter un truc de merde
+    //tu le vois ?
     w.show();
 
     return a.exec();
