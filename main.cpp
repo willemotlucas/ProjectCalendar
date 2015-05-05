5a2b55af8f21aa4f735b2e19db09f1c91f5fd3c7
willemotlucas <willemotlucas@gmail.com>
2015-05-05 19:57:20 +0200

    test

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main(int argc, char *argv[])
     MainWindow w;
     //histoire de rajouter un truc de merde
     //tu le vois ?
+    //oui je le vois petit pd
     w.show();
 
     return a.exec();
