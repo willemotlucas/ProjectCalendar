9e0e03d0b3d802e2bd487fbf5f28eb8dc1974448
willemotlucas <willemotlucas@gmail.com>
2015-05-05 20:10:27 +0200

    remove test comments

--- main.cpp
+++ main.cpp
@@ -5,9 +5,6 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    //histoire de rajouter un truc de merde
-    //tu le vois ?
-    //oui je le vois petit pd
     w.show();
 
     return a.exec();
